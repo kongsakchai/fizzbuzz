97a8a0653c2f8d31e7d2b85dcd599457f4f31dad
kongsakchai <ksakchai.dev@gmail.com>
2024-07-28 23:45:27 +0700

    :white_check_mark: test: case 2 got 2

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -9,5 +9,8 @@ func main() {
 }
 
 func FizzBuzz(n int) string {
+	if n == 2 {
+		return "2"
+	}
 	return "1"
 }
--- fizzbuzz/main_test.go
+++ fizzbuzz/main_test.go
@@ -2,10 +2,24 @@ package main
 
 import "testing"
 
-func Test(t *testing.T) {
+func TestCase1(t *testing.T) {
 	// Arrange
 	give := 1
 	want := "1"
+
+	// Act
+	got := FizzBuzz(give)
+
+	// Assert
+	if want != got {
+		t.Errorf("give %d expected %s, actual %s", give, want, got)
+	}
+}
+
+func TestCase2(t *testing.T) {
+	// Arrange
+	give := 2
+	want := "2"
 
 	// Act
 	got := FizzBuzz(give)
