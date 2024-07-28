e1f435d9e0dcd04f78d6d3e3f414d8ba5f500298
kongsakchai <ksakchai.dev@gmail.com>
2024-07-28 23:45:27 +0700

    :white_check_mark: test: case 1 got 1

--- fizzbuzz/main_test.go
+++ fizzbuzz/main_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	// Arrange
+	give := 1
+	want := "1"
+
+	// Act
+	got := FizzBuzz(give)
+
+	// Assert
+	if want != got {
+		t.Errorf("give %d expected %s, actual %s", give, want, got)
+	}
+}
