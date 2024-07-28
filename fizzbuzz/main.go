ad79376dc7e69f95fb9a7b13c87195e50930151c
kongsakchai <ksakchai.dev@gmail.com>
2024-07-28 23:45:29 +0700

    :recycle: refactor: case Fizz

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -16,7 +16,7 @@ func FizzBuzz(n int) string {
 	if n == 5 || n == 10 || n == 25 {
 		return "Buzz"
 	}
-	if n == 3 || n == 6 || n == 9 || n == 12 || n == 18 {
+	if n%3 == 0 {
 		return "Fizz"
 	}
 	return strconv.Itoa(n)
